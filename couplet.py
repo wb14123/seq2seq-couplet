6f33b070687a72c292ee00ec69db3acba7049cf0
Bin Wang <wbin00@gmail.com>
2017-09-15 18:26:43 +0800

    params

---
+++ couplet.py
@@ -0,0 +1,13 @@
+
+from model import Model
+
+m = Model(
+        '/data/dl-data/couplet/train/in.txt',
+        '/data/dl-data/couplet/train/out.txt',
+        '/data/dl-data/couplet/test/in.txt',
+        '/data/dl-data/couplet/test/out.txt',
+        '/data/dl-data/couplet/vocabs',
+        num_units=1024, layers=4, dropout=0.2,
+        batch_size=32, learning_rate=0.001, output_dir='./couplet')
+
+m.train(5000000)
--- nmt.py
+++ nmt.py
@@ -6,8 +6,8 @@ m = Model(
         '/data/dl-data/wmt-2016/train.tok.clean.bpe.32000.en',
         '/data/dl-data/wmt-2016/newstest2016.tok.de',
         '/data/dl-data/wmt-2016/newstest2016.tok.en',
-        '/data/dl-data/wmt-2016/vocab.bpe.32000',
+        '/data/dl-data/wmt-2016/vocab.bpe.32000.bk',
         num_units=1024, layers=4, dropout=0.2,
-        batch_size=32, learning_rate=0.001, output_dir='./output')
+        batch_size=32, learning_rate=0.001, output_dir='./output_nmt')
 
 m.train(5000000)
