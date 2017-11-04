2624f0ae85dcbddae0fb9c7e7103914d4bc03a2c
Bin Wang <wbin00@gmail.com>
2017-11-04 11:49:42 +0800

    add log

--- server.py
+++ server.py
@@ -3,12 +3,13 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from model import Model
 from gevent.wsgi import WSGIServer
+import logging
 
 app = Flask(__name__)
 CORS(app)
 
 vocab_file = '/data/dl-data/couplet/vocabs'
-model_dir = '/data/dl-data/models/tf-lib/output_couplet'
+model_dir = '/data/dl-data/models/tf-lib/output_couplet_prod'
 
 m = Model(
         None, None, None, None, vocab_file,
@@ -20,11 +21,12 @@ m = Model(
 
 @app.route('/chat/couplet/<in_str>')
 def chat_couplet(in_str):
-    if len(in_str) == 0 or len(in_str) > 20:
+    if len(in_str) == 0 or len(in_str) > 50:
         output = u'您的输入太长了'
     else:
         output = m.infer(' '.join(in_str))
         output = ''.join(output.split(' '))
+    print('上联：%s；下联：%s' % (in_str, output))
     return jsonify({'output': output})
 
 
