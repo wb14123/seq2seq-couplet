143edae8efd2c470aa0b5746e5b65daff4b2f7cc
Bin Wang <bin.wang@mail.binwang.me>
2021-07-08 20:11:23 -0400

    add score based on char match

--- server.py
+++ server.py
@@ -38,30 +38,50 @@ m = Model(
         output_dir=model_dir,
         restore_model=True, init_train=False, init_infer=True)
 
+def manual_correct_result(in_str, outputs, scores):
+    for i in range(len(outputs)):
+        output = outputs[i]
+        scores[i] -= abs(len(in_str) - len(output))
+        length = min(len(in_str), len(output))
+        for j in range(length):
+            for k in range(j, length):
+                if (in_str[j] == in_str[k]) != (output[j] == output[k]):
+                    scores[i] -= 1
+                if (in_str[j] == output[k]):
+                    scores[i] -= 1
+    return outputs, scores
+
+
+def sort_outputs(outputs, scores):
+    new_scores, new_outputs = zip(*sorted(zip(scores, outputs), reverse=True))
+    return list(new_outputs), list(new_scores)
+
+
+def infer(in_str):
+    if len(in_str) == 0 or len(in_str) > 50:
+        return [u'您的输入太长了'], []
+    else:
+        model_outputs, model_scores = m.infer(' '.join(in_str))
+        model_scores = model_scores.tolist()
+        unsorted_outputs, unsorted_scores = manual_correct_result(
+                in_str, model_outputs, model_scores)
+        output, score = sort_outputs(unsorted_outputs, unsorted_scores)
+        logging.info('上联：%s；下联：%s ; score: %s' % (
+            in_str, output, score))
+        return output, score
+
+
 @app.route('/chat/couplet/<in_str>')
 def chat_couplet(in_str):
-    if len(in_str) == 0 or len(in_str) > 50:
-        output = u'您的输入太长了'
-    else:
-        result = {'text': []}
-        output, score = m.infer(' '.join(in_str))
-        score = score.tolist()
-    logging.info('上联：%s；下联：%s ; score: %s' % (
-        in_str, output, score))
+    output, score = infer(in_str)
     return jsonify({'output': output[0]})
 
 
 @app.route('/v0.2/couplet/<in_str>')
 def chat_couplet_v2(in_str):
-    if len(in_str) == 0 or len(in_str) > 50:
-        output = u'您的输入太长了'
-    else:
-        result = {'text': []}
-        output, score = m.infer(' '.join(in_str))
-        score = score.tolist()
-    logging.info('上联：%s；下联：%s ; score: %s' % (
-        in_str, output, score))
+    output, score = infer(in_str)
     return jsonify({'output': output, 'score': score})
+
 
 http_server = WSGIServer(('', 5000), app)
 http_server.serve_forever()
